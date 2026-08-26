61d2885f87229e6f4472d26ed5bfcc869551a230
Ygor Amaro <ygor.amaro@belasartes.br>
2026-08-26 11:33:57 -0300

    update

--- regressao.py
+++ regressao.py
@@ -1,4 +1,4 @@
-# %%
+# %% importação dos dados
 import pandas as pd
 
 data = "data/dados_cerveja_nota.xlsx"
@@ -9,6 +9,7 @@ print(df.head())
 # %%
 
 from sklearn import linear_model
+from sklearn import tree
 
 X = df[['cerveja']]
 y = df[['nota']]
@@ -16,16 +17,22 @@ y = df[['nota']]
 reg = linear_model.LinearRegression()
 reg.fit(X, y)
 
-# %%
-
 a, b = reg.intercept_, reg.coef_[0]
-
-
 print(a, b)
 
-predict = reg.predict(X.drop_duplicates())
+predict_reg = reg.predict(X.drop_duplicates())
 
-# %%
+seed = 42
+arvore_full = tree.DecisionTreeRegressor(random_state=seed)
+arvore_full.fit(X, y)
+predict_arvore_full = arvore_full.predict(X.drop_duplicates())
+
+arvore_d2 = tree.DecisionTreeRegressor(random_state=seed, 
+                                       max_depth=2)
+arvore_d2.fit(X, y)
+predict_arvore_d2 = arvore_d2.predict(X.drop_duplicates())
+
+# %% Gráfico de regressão linear
 
 import matplotlib.pyplot as plt
 
@@ -35,7 +42,14 @@ plt.title("Relação Cerveja x Notas")
 plt.xlabel("Cerveja")
 plt.ylabel("Nota")
 
-plt.plot(X.drop_duplicates()['cerveja'], predict)
-plt.legend(['Observado', f'y = {a.round(3)} + {b.round(3)} x'])
+plt.plot(X.drop_duplicates()['cerveja'], predict_reg)
+plt.plot(X.drop_duplicates()['cerveja'], predict_arvore_full)
+plt.plot(X.drop_duplicates()['cerveja'], predict_arvore_d2)
+
+plt.legend(['Observado', 
+            f'y = {a.round(3)} + {b.round(3)} x',
+            'Árvore Full',
+            'Árvore Depth 2'
+            ])
 
 # %%
